96f73d6e27f0383b5affe6fcb365166a3405183c
Igor Meszaros <igor.mesaros@bighand.com>
2024-01-26 21:55:14 +0000

    Add Employee Overview with mock data

---
+++ BethanyPieShopHRM/BethanyPieShopHRM/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,14 @@
+﻿using BethanysPieShopHRM.App.Models;
+using BethanysPieShopHRM.Shared.Domain;
+
+namespace BethanyPieShopHRM.Pages;
+
+public partial class EmployeeOverview
+{
+    public List<Employee>? Employees { get; set; } = default;
+
+    protected override void OnInitialized()
+    {
+        Employees = MockDataService.Employees;
+    }
+}
