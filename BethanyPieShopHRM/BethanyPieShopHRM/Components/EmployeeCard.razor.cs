821a55b08b86aa85771b5d9e05705ba23151f932
Igor Meszaros <igor.mesaros@bighand.com>
2024-01-28 21:46:36 +0000

    Create employee card

---
+++ BethanyPieShopHRM/BethanyPieShopHRM/Components/EmployeeCard.razor.cs
@@ -0,0 +1,10 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanyPieShopHRM.Components;
+
+public partial class EmployeeCard
+{
+    [Parameter]
+    public Employee Employee { get; set; } = default!;
+}
