0fcd5497b86453db570f33dcd831df4d616c362f
Igor Meszaros <igor.mesaros@bighand.com>
2024-01-30 20:56:25 +0000

    Added Quick View popup

--- BethanyPieShopHRM/BethanyPieShopHRM/Components/EmployeeCard.razor.cs
+++ BethanyPieShopHRM/BethanyPieShopHRM/Components/EmployeeCard.razor.cs
@@ -7,4 +7,7 @@ public partial class EmployeeCard
 {
     [Parameter]
     public Employee Employee { get; set; } = default!;
+
+    [Parameter]
+    public EventCallback<Employee> EmployeeQuickViewClicked { get; set; }
 }
--- BethanyPieShopHRM/BethanyPieShopHRM/Pages/EmployeeOverview.razor.cs
+++ BethanyPieShopHRM/BethanyPieShopHRM/Pages/EmployeeOverview.razor.cs
@@ -7,8 +7,15 @@ public partial class EmployeeOverview
 {
     public List<Employee>? Employees { get; set; } = default;
 
+    private Employee? _selectedEmployee;
+
     protected override void OnInitialized()
     {
         Employees = MockDataService.Employees;
+    }
+
+    public void ShowQuickViewPopup(Employee selectedEmployee)
+    {
+        _selectedEmployee = selectedEmployee;
     }
 }
