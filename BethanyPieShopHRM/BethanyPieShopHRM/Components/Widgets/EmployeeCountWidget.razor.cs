3ef73f7bd1d1a01c58a0c13fa638bf41a30728ae
Igor Meszaros <igor.mesaros@bighand.com>
2024-02-02 20:52:23 +0000

    Dynamic Component

---
+++ BethanyPieShopHRM/BethanyPieShopHRM/Components/Widgets/EmployeeCountWidget.razor.cs
@@ -0,0 +1,14 @@
+﻿using BethanysPieShopHRM.App.Models;
+
+namespace BethanyPieShopHRM.Components.Widgets
+{
+    public partial class EmployeeCountWidget
+    {
+        public int EmployeeCounter { get; set; }
+
+        protected override void OnInitialized()
+        {
+            EmployeeCounter = MockDataService.Employees.Count;
+        }
+    }
+}
