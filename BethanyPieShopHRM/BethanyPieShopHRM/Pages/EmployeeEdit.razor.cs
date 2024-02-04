14426082a622000872ca77bd13b82ca0ef315ad2
Igor Meszaros <igor.mesaros@bighand.com>
2024-02-04 21:34:49 +0000

    Create basic Edit page with EditForm and input text

---
+++ BethanyPieShopHRM/BethanyPieShopHRM/Pages/EmployeeEdit.razor.cs
@@ -0,0 +1,21 @@
+﻿using BethanyPieShopHRM.Services;
+using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanyPieShopHRM.Pages;
+
+public partial class EmployeeEdit
+{
+    [Inject]
+    public IEmployeeDataService? EmployeeDataService { get; set; }
+
+    [Parameter]
+    public string? EmployeeId { get; set; }
+
+    public Employee Employee { get; set; } = new Employee();
+
+    protected async override Task OnInitializedAsync()
+    {
+        Employee = await EmployeeDataService.GetEmployeeDetailsAsync(int.Parse(EmployeeId));
+    }
+}
