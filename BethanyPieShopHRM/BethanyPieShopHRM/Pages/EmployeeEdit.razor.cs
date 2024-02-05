144f7d4ca99506094767056a0b8c8a09b78bf072
Igor Meszaros <igor.mesaros@bighand.com>
2024-02-05 20:57:01 +0000

    Add, Update, Delete Emloyee

--- BethanyPieShopHRM/BethanyPieShopHRM/Pages/EmployeeEdit.razor.cs
+++ BethanyPieShopHRM/BethanyPieShopHRM/Pages/EmployeeEdit.razor.cs
@@ -15,6 +15,9 @@ public partial class EmployeeEdit
     [Inject]
     public IJobCategoryDataService? JobCategoryDataService { get; set; }
 
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
     [Parameter]
     public string? EmployeeId { get; set; }
 
@@ -23,8 +26,13 @@ public partial class EmployeeEdit
     public List<Country> Countries { get; set; } = new List<Country>();
     public List<JobCategory> JobCategories { get; set; } = new List<JobCategory>();
 
+    protected string Message = string.Empty;
+    protected string StatusClass = string.Empty;
+    protected bool Saved;
+
     protected async override Task OnInitializedAsync()
     {
+        Saved = false;
         JobCategories = (await JobCategoryDataService.GetJobCategoriesAsync()).ToList();
         Countries = (await CountryService.GetAllCountriesAsync()).ToList();
 
@@ -37,4 +45,55 @@ public partial class EmployeeEdit
             Employee = new Employee {CountryId = 1, JobCategoryId = 1, BirthDate = DateTime.Now, JoinedDate = DateTime.Now};
         }
     }
+
+    protected async Task HandleValidSubmit()
+    {
+        Saved = false;
+
+        if (Employee.EmployeeId == 0) //new
+        {
+            var addedEmployee = await EmployeeDataService.AddEmployeeAsync(Employee);
+            if (addedEmployee != null)
+            {
+                StatusClass = "alert-success";
+                Message = "New employee added successfully.";
+                Saved = true;
+            }
+            else
+            {
+                StatusClass = "alert-danger";
+                Message = "Something went wrong adding the new employee. Please try again.";
+                Saved = false;
+            }
+        }
+        else
+        {
+            await EmployeeDataService.UpdateEmployeeAsync(Employee);
+            StatusClass = "alert-success";
+            Message = "Employee updated successfully.";
+            Saved = true;
+        }
+    }
+
+    protected async Task HandleInvalidSubmit()
+    {
+        StatusClass = "alert-danger";
+        Message = "There are some validation errors. Please try again.";
+    }
+
+    protected async Task DeleteEmployee()
+    {
+        await EmployeeDataService.DeleteEmployeeAsync(Employee.EmployeeId);
+
+        StatusClass = "alert-success";
+        Message = "Deleted successfully";
+
+        Saved = true;
+    }
+
+    protected void NavigateToOverview()
+    {
+        NavigationManager.NavigateTo("/employeeoverview");
+    }
+
 }
