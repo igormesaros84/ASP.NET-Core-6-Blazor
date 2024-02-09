c9118919548671f069b75b997564ab6abe6a4a10
Igor Meszaros <igor.mesaros@bighand.com>
2024-02-09 21:25:06 +0000

    Display employee location using Javascript

---
+++ BethanyPieShopHRM/BethanyPieShopHRM/Components/Map.razor.cs
@@ -0,0 +1,27 @@
+﻿using BethanysPieShopHRM.Shared.Model;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BethanyPieShopHRM.Components;
+
+public partial class Map
+{
+    string elementId = $"map-{Guid.NewGuid():D}";
+
+    [Inject]
+    public IJSRuntime JSRuntime { get; set; }
+
+    [Parameter]
+    public double Zoom { get; set; }
+
+    [Parameter]
+    public List<Marker> Markers { get; set; }
+
+    // It is important to run all Javascript interrop after all the html has been rendered
+    protected async override Task OnAfterRenderAsync(bool firstRender)
+    {
+        await JSRuntime.InvokeVoidAsync(
+            "deliveryMap.showOrUpdate",
+            elementId, Markers);
+    }
+}
--- BethanyPieShopHRM/BethanyPieShopHRM/Pages/EmployeeDetail.razor.cs
+++ BethanyPieShopHRM/BethanyPieShopHRM/Pages/EmployeeDetail.razor.cs
@@ -1,6 +1,7 @@
 ﻿using BethanyPieShopHRM.Services;
 using BethanysPieShopHRM.App.Models;
 using BethanysPieShopHRM.Shared.Domain;
+using BethanysPieShopHRM.Shared.Model;
 using Microsoft.AspNetCore.Components;
 
 namespace BethanyPieShopHRM.Pages;
@@ -14,9 +15,25 @@ public partial class EmployeeDetail
     public string EmployeeId {get; set; }
 
     public Employee? Employee { get; set; } = new Employee();
+    
+    public List<Marker> MapMarkers { get; set; } = new List<Marker>();
 
     protected override async Task OnInitializedAsync()
     {
         Employee = (await EmployeeDataService.GetEmployeeDetailsAsync(int.Parse(EmployeeId)));
+
+        if ( Employee.Longitude.HasValue && Employee.Latitude.HasValue )
+        {
+            MapMarkers = new List<Marker>
+            {
+                new Marker
+                {
+                    Description =  $"{Employee.FirstName} {Employee.LastName}",
+                    ShowPopup = false,
+                    X = Employee.Longitude.Value,
+                    Y = Employee.Latitude.Value,
+                }
+            };
+        }
     }
 }
