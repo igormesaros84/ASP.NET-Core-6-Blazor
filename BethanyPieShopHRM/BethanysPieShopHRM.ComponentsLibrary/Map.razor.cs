d118e30d381127121f4b03be76789c8780856b14
Igor Meszaros <igor.mesaros@bighand.com>
2024-02-09 21:37:57 +0000

    Moved the Map component to a Razor Class Library

--- BethanyPieShopHRM/BethanysPieShopHRM.ComponentsLibrary/Map.razor.cs
+++ BethanyPieShopHRM/BethanysPieShopHRM.ComponentsLibrary/Map.razor.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 
-namespace BethanyPieShopHRM.Components;
+namespace BethanysPieShopHRM.ComponentsLibrary;
 
 public partial class Map
 {
